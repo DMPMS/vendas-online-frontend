909281f4b89b489bd3a281f90a96b15b7c5775ff
Davi Monteiro <monteiro_davi@yahoo.com>
2024-03-01 16:34:11 -0300

    Subindo correções do pre-commit (deveria ter ido com o commit anterior)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   semi: true, // Adiciona ponto e vírgula onde necessário
-  trailingComma: "all", // Coloca vírgula no último de uma lista
+  trailingComma: 'all', // Coloca vírgula no último de uma lista
   singleQuote: true, // Utiliza aspas simples ao invés de aspas duplas
   printWidth: 100, // Quebra a linha ao chegar em 100 caracteres
   tabWidth: 2, // Tamanho da tab
