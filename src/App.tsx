757522e741413a54bd310cce055426562066e9af
Davi Monteiro <monteiro_davi@yahoo.com>
2024-03-20 10:34:20 -0300

    Criando módulo de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Importei aqui para contornar o problema, para rodar a aplicação, tem que remover essa linha
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 
 import type { Router as RemixRouter } from '@remix-run/router';
 import { useEffect } from 'react';
@@ -8,6 +8,7 @@ import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-d
 import { categoryScreens } from './modules/category/routes';
 import { firstScreenRoutes } from './modules/firstScreen/routes';
 import { loginRoutes } from './modules/login/routes';
+import { orderScreens } from './modules/orders/routes';
 import { productScreens } from './modules/product/routes';
 import { URL_USER } from './shared/constants/urls';
 import { MethodsEnum } from './shared/enums/methods.enum';
@@ -21,6 +22,7 @@ const routesLoggedIn: RouteObject[] = [
   ...productScreens,
   ...categoryScreens,
   ...firstScreenRoutes,
+  ...orderScreens,
 ].map((route) => ({
   ...route,
   loader: verifyLoggedIn,
