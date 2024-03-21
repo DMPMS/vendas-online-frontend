24bc5dfa373774703407683d4c7564abe7a3f9af
Davi Monteiro <monteiro_davi@yahoo.com>
2024-03-21 13:09:54 -0300

    Criando tela de listagem de usuários

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { firstScreenRoutes } from './modules/firstScreen/routes';
 import { loginRoutes } from './modules/login/routes';
 import { orderScreens } from './modules/orders/routes';
 import { productScreens } from './modules/product/routes';
+import { userScreens } from './modules/user/routes';
 import { URL_USER } from './shared/constants/urls';
 import { MethodsEnum } from './shared/enums/methods.enum';
 import { getAuthorizationToken, verifyLoggedIn } from './shared/functions/connection/auth';
@@ -22,6 +23,7 @@ const routesLoggedIn: RouteObject[] = [
   ...productScreens,
   ...categoryScreens,
   ...firstScreenRoutes,
+  ...userScreens,
   ...orderScreens,
 ].map((route) => ({
   ...route,
